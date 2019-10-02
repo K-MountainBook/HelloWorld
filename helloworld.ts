6e40da569cd870e39f29489f961deef3b0e43373
tisk77597 <yamamoto.keita@tis.co.jp>
2019-10-02 15:17:17 +0900

    JSを作ってみる

--- helloworld.ts
+++ helloworld.ts
@@ -1,2 +1,19 @@
-var message: string = 'Hello World';
-console.log(message);
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+document.body.textContent = greeter(user);
